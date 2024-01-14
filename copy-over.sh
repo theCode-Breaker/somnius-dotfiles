1cb17b4afa866b11e83f61e92ef809217a0e3b4c
Lefteris Iliadis <lefteros@outlook.com>
2024-01-14 15:43:34 +0200

    dotfile backup script

---
+++ copy-over.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "This scripts copies over the dotfiles and misc requirements"
+echo " "
+read -p "Press [Enter] key to start backup..."
+read -p "Press any key to resume ..."
+## Bash add pause prompt for 5 seconds ##
+read -t 5 -p "I am going to wait for 5 seconds only ..."
