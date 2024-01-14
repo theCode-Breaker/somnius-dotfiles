0c428039698c89c4c1974d26bd02fcd03958c458
Lefteris Iliadis <lefteros@outlook.com>
2024-01-14 16:48:54 +0200

    v0.1 deploy

--- backup.sh
+++ backup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+clear
 echo "This scripts backs up the dotfiles and misc requirements"
 echo " "
 read -p "Press [Enter] key to start backup..."
@@ -6,45 +7,27 @@ read -p "Press [Enter] key to start backup..."
 echo " "
 read -t 5 -p "I am going to wait Ctrl+C for only 5 seconds...🛑"
 echo " "
-echo "[COPY] ~/.bashrc to ./bashrc"
+
 cp ~/.bashrc ./bashrc
-echo " "
-echo "[COPY] ~/.config/rofi to ./config/rofi"
-cp -r ~/.config/rofi ./config/rofi
-echo " "
-[ -d "./config/hypr/" ] && echo "[STAT] ./config/hypr/ exists." || echo "[ERRR] Directory ./config/hypr/ does NOT exist, creating." && mkdir ./config/hypr
-echo " "
-echo "[COPY] ~/.config/hypr/scripts to ./config/hypr/scripts"
+
+[ -d "./config" ] && echo "[STAT] ./config exists" || echo "[ERRR] Directory ./config does NOT exist, creating." && mkdir ./config
+
+[ -d "./config/rofi" ] && echo "[STAT] ./config/rofi/ exists, removing.." && rm -rf ./config/rofi || echo "[ERRR] Directory ./config/rofi/ does NOT exist, creating." && mkdir ./config/rofi
+cp -r ~/.config/rofi/config.rasi ./config/rofi/config.rasi
+
+[ -d "./config/hypr/" ] && echo "[STAT] ./config/hypr/ exists" || echo "[ERRR] Directory ./config/hypr/ does NOT exist, creating." && mkdir ./config/hypr
+
+cp -r ~/.config/hypr/rofi ./config/hypr/rofi
 cp -r ~/.config/hypr/scripts ./config/hypr/scripts
-echo " "
-echo "[COPY] ~/.config/hypr/theme to ./config/hypr/theme"
 cp -r ~/.config/hypr/theme ./config/hypr/theme
-echo " "
-echo "[COPY] ~/.config/hypr/wallpapers to ./config/hypr/wallpapers"
 cp -r ~/.config/hypr/wallpapers ./config/hypr/wallpapers
-echo " "
-echo "[COPY] ~/.config/hypr/waybar to ./config/hypr/waybar"
 cp -r ~/.config/hypr/waybar ./config/hypr/waybar
-echo " "
-echo "[COPY] ~/.config/hypr/wlogout to ./config/hypr/wlogout"
 cp -r ~/.config/hypr/wlogout ./config/hypr/wlogout
-echo " "
-echo "[COPY] ~/.config/hypr/wofi to ./config/hypr/wofi"
 cp -r ~/.config/hypr/wofi ./config/hypr/wofi
-echo " "
-echo "[COPY] ~/.config/hypr/hyprland.conf to ./config/hypr/hyprland.conf"
+
 cp -r ~/.config/hypr/hyprland.conf ./config/hypr/hyprland.conf
-echo " "
-echo "[COPY] ~/.config/hypr/hyprtheme.conf to ./config/hypr/hyprtheme.conf"
 cp -r ~/.config/hypr/hyprtheme.conf ./config/hypr/hyprtheme.conf
-echo " "
-echo "[COPY] ~/.config/rofi to ./config/rofi"
-cp -r ~/.config/rofi ./config/rofi
 
 echo -ne '\007' #pc speaker beep
-echo " "
 echo "--------------------------------------------------------------"
-echo " "
 echo "All files and folders have been backed up."
-echo " "
-echo " "
--- install.sh
+++ install.sh
@@ -1,7 +1,28 @@
 #!/bin/bash
-echo "This scripts copies over the dotfiles and misc requirements to proper folders"
+clear
+echo "This scripts backs up the dotfiles and misc requirements"
 echo " "
 read -p "Press [Enter] key to start backup..."
 ## Bash add pause prompt for 5 seconds ##
+echo " "
 read -t 5 -p "I am going to wait Ctrl+C for only 5 seconds...🛑"
 echo " "
+
+cp ./bashrc ~/.bashrc
+
+cp -r ./config/rofi/config.rasi ~/.config/rofi/config.rasi
+
+cp -r ./config/hypr/rofi ~/.config/hypr/
+cp -r ./config/hypr/scripts ~/.config/hypr/
+cp -r ./config/hypr/theme ~/.config/hypr/
+cp -r ./config/hypr/wallpapers ~/.config/hypr/
+cp -r ./config/hypr/waybar ~/.config/hypr/
+cp -r ./config/hypr/wlogout ~/.config/hypr/
+cp -r ./config/hypr/wofi ~/.config/hypr/
+
+cp -r ./config/hypr/hyprland.conf ~/.config/hypr/hyprland.conf
+cp -r ./config/hypr/hyprtheme.conf ~/.config/hypr/hyprtheme.conf
+
+echo -ne '\007' #pc speaker beep
+echo "--------------------------------------------------------------"
+echo "All files and folders have been copied over in proper places."
