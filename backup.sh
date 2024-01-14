12c93d1f0c63656b969d253aa644bcbfa0f8b00b
Lefteris Iliadis <lefteros@outlook.com>
2024-01-14 15:54:04 +0200

    backup script init install script init

---
+++ backup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "This scripts backs up the dotfiles and misc requirements"
+echo " "
+read -p "Press [Enter] key to start backup..."
+## Bash add pause prompt for 5 seconds ##
+read -t 5 -p "I am going to wait Ctrl+C for only 5 seconds...🛑"
---
+++ install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "This scripts copies over the dotfiles and misc requirements to proper folders"
+echo " "
+read -p "Press [Enter] key to start backup..."
+## Bash add pause prompt for 5 seconds ##
+read -t 5 -p "I am going to wait Ctrl+C for only 5 seconds...🛑"
