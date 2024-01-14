74bdcd10b79bf9b68ae3eda1740968e868c524fd
Lefteris Iliadis <lefteros@outlook.com>
2024-01-14 20:02:01 +0200

    backup zips script on install.sh

--- install.sh
+++ install.sh
@@ -7,7 +7,14 @@ read -p "Press [Enter] key to start backup..."
 echo " "
 read -t 5 -p "I am going to wait Ctrl+C for only 5 seconds...🛑"
 echo " "
-
+echo " "
+echo "Creating a zip of the installed files before replacing them..."
+zip -r -9 -q ~/dotfiles_backup_$(date +"%Y-%m-%d_%H-%M").zip ~/.bashrc ~/.config/rofi ~/.config/hypr/rofi ~/.config/hypr/scripts ~/.config/hypr/theme ~/.config/hypr/wallpapers ~/.config/hypr/waybar ~/.config/hypr/wlogout ~/.config/hypr/wofi /.config/hypr/hyprland.conf ~/.config/hypr/hyprtheme.conf
+echo " "
+echo "Current Backups"
+du -sh ~/dotfiles_backup*.zip
+echo " "
+read -t 5 -p "Now installing files into places. I am going to wait Ctrl+C for only 5 seconds...🛑"
 cp ./bashrc ~/.bashrc
 
 cp -r ./config/rofi/config.rasi ~/.config/rofi/config.rasi
@@ -24,5 +31,6 @@ cp -r ./config/hypr/hyprland.conf ~/.config/hypr/hyprland.conf
 cp -r ./config/hypr/hyprtheme.conf ~/.config/hypr/hyprtheme.conf
 
 echo -ne '\007' #pc speaker beep
+echo " "
 echo "--------------------------------------------------------------"
 echo "All files and folders have been copied over in proper places."
